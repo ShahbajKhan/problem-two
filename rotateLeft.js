0698c0b3f52975078a98660cdd4bb620b9d456f0
ShahbajKhan <shktoky@gmail.com>
2021-06-21 14:56:19 +0600

    handled if array is empty

--- rotateLeft.js
+++ rotateLeft.js
@@ -1,9 +1,17 @@
 const rotateLeft = (n, d, arr)=>{
-    
+    if(d<0){
+        console.log("Enter positive value for d");
+        return;
+    }
+    if(!arr.length){
+        console.log("Array is empty")
+        return;
+    }
     for (let i = 0; i < d; i++) {
         let firstElement = arr.shift();
         arr.push(firstElement);      
     }
     console.log(arr);
+    return arr;
 }
 rotateLeft(5, 4, [1,2,3,4,5]);
